d4fa49a634e57abcf8b1ffe218496fdb9ce0d90d
Juan Domínguez <juan.dominguezr@udc.es>
2022-09-18 17:53:16 +0200

    added P1

---
+++ p1/e1/nombre.ml
@@ -0,0 +1,2 @@
+print_endline("Juan Domínguez Rodríguez"); 
+print_endline("juan.dominguezr@udc.es"); 
---
+++ p1/e3/expr2.ml
@@ -0,0 +1,6 @@
+let u = 2/2+3-1*8;;
+let v = sqrt(2.);;
+let w = char_of_int 66;;
+let x = not true;;
+let y = if(x) then "true" else "false";; 
+print_endline;;
