f939562bb679b08074646ae49daab7c630b81329
Juan Domínguez <juan.dominguezr@udc.es>
2022-09-22 09:48:33 +0200

    upgraded e3

--- p2/e3/fact.ml
+++ p2/e3/fact.ml
@@ -2,5 +2,5 @@ let rec fact = function
 0 -> 1
 | n -> n * fact (n - 1);;
 
-if(Array.length(Sys.argv)!=2) then print_endline("Número de argumentos inválido") else
-print_endline(string_of_int(fact(int_of_string(Sys.argv.(1)))));
+if(Array.length(Sys.argv) <> 2) then print_endline("Número de argumentos inválido") else
+print_endline (string_of_int (fact (int_of_string (Sys.argv.(1)))));
---
+++ p2/e3/fact2.ml
@@ -0,0 +1,8 @@
+let rec fact = function
+0 -> 1
+| n -> n * fact (n - 1)
+in if(Array.length(Sys.argv) <> 2) 
+then print_endline("Número de argumentos inválido") else
+print_endline (string_of_int (fact (int_of_string (Sys.argv.(1)))))
+
+
