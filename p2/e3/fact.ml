d37cc194463f2a57556044838581859860603bd1
Juan Domínguez <juan.dominguezr@udc.es>
2022-09-22 09:23:34 +0200

    added p2

---
+++ p2/e3/fact.ml
@@ -0,0 +1,6 @@
+let rec fact = function
+0 -> 1
+| n -> n * fact (n - 1);;
+
+if(Array.length(Sys.argv)!=2) then print_endline("Número de argumentos inválido") else
+print_endline(string_of_int(fact(int_of_string(Sys.argv.(1)))));
