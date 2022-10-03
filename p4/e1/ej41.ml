0ddc5dc6dd37e42000c8bc4dee7266f9204a8586
Juan Domínguez <juan.dominguezr@udc.es>
2022-10-03 10:54:41 +0200

    finished e1

--- p4/e1/ej41.ml
+++ p4/e1/ej41.ml
@@ -14,3 +14,12 @@ let rec exp10 n = (*n>=0*)
 (*4-*)
 let rec reverse n = (*n>=0*)
 	if n >= 10 then (exp10((num_cifras n) - 1))*(n mod 10) + reverse (n/10) else (n mod 10);; 
+
+(*5-*)
+let palindromo s = 
+	let rec check_from i =
+	if i < (String.length s)-1-i then
+	if s.[i] != s.[(String.length s)-1-i] then false
+	else check_from(i+1) else true
+	in check_from 0;;
+	
