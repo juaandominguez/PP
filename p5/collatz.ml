48ab81d4175ee99c6eb0e8115cc5b7a82c5b007e
Juan Domínguez <juan.dominguezr@udc.es>
2022-10-12 21:11:10 +0200

    add length and top

--- p5/collatz.ml
+++ p5/collatz.ml
@@ -3,3 +3,14 @@ let f n = if n mod 2 = 0 then n / 2 else 3 * n + 1;;
 let rec orbit n =
 	if n = 1 then string_of_int(n)
 	else String.concat ", " [string_of_int(n); orbit(f n)];;
+
+let rec length n = 
+	if n = 1 then 0
+	else 1 + length(f n);; 
+
+let rec top n = 
+	if n = 1 then 1
+		else max n (top(f n));;
+
+
+
