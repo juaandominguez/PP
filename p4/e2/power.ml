2ed3337dc58135524f8e24d00eb87325ddba3781
Juan Dominguez <juan.dominguezr@udc.es>
2022-10-15 18:28:44 +0200

    fix bugs

--- p4/e2/power.ml
+++ p4/e2/power.ml
@@ -6,6 +6,8 @@ let rec power' x y = (*y>=0*)
 	if y=0 then 1 else
 	if y mod 2 = 0 then power' (x*x) (y/2) else
 	x * power' (x*x) (y/2);;
+(*power' es mas eficiente que power ya que reduce el número de recursiones necesarias,
+pero dentro del rango de los int la diferencia en esta funcion no es percetible*)
 
 let rec powerf x y = (*y>=0*)
 	if y=0 then 1. else
--- p4/e3/mcd.ml
+++ p4/e3/mcd.ml
@@ -1,3 +1,4 @@
 let rec mcd (x,y) = 
 	if y>x then mcd (y,x) else
+	if x = 0 then y else
 	if y > 0 then mcd ((x mod y),y) else x;;
