00aedd09acce5c2c29d4373819306ca43f4d3889
Juan Domínguez <juan.dominguezr@udc.es>
2022-10-20 10:27:59 +0200

    upload P6

--- p6/powmod.ml
+++ p6/powmod.ml
@@ -6,7 +6,19 @@ let rec power' x y = (*y>=0*)
 (*let powmod m b e = power' b e mod m;;*)
 
 let rec powmod m b e = 
+        if e=0 then 1 mod m
+        else
+        let 
         if (e>2) then (powmod m b e-2) * (powmod m b e-1)mod m
         else power' b e mod m;;
 
 
+
+let rec powmod m b e = 
+        if e=0 then 1 mod m
+        else
+        let p = powmod m ((b*b)mod m) (e/2) in
+        if e mod 2=0 then p
+        else b*p mod m
+
+let powmod m b e = powmod m (b mod m) e;;
